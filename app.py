23edfdcda70cd0a644ce198994b08cd973ad83a5
shahil1993 <shahil1993@gmail.com>
2017-10-12 14:46:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,8 +15,8 @@
 # limitations under the License..
 
 from __future__ import print_function
-#from future.standard_library import install_aliases
-#install_aliases()
+from future.standard_library import install_aliases
+install_aliases()
 
 from urllib.parse import urlparse, urlencode
 from urllib.request import urlopen, Request
@@ -24,9 +24,6 @@ from urllib.error import HTTPError
 
 import json
 import os
-import io
-import requests
-import pandas as pd
 
 from flask import Flask
 from flask import request
@@ -43,29 +40,66 @@ def webhook():
     print("Request:")
     print(json.dumps(req, indent=4))
 
-    res = makeWebhookResult(req)
+    res = processRequest(req)
 
     res = json.dumps(res, indent=4)
-    print(res)
+    # print(res)
     r = make_response(res)
     r.headers['Content-Type'] = 'application/json'
     return r
 
-def makeWebhookResult(req):
-    if req.get("result").get("action") != "cont":
+
+def processRequest(req):
+    if req.get("result").get("action") != "yahooWeatherForecast":
         return {}
+    baseurl = "https://query.yahooapis.com/v1/public/yql?"
+    yql_query = makeYqlQuery(req)
+    if yql_query is None:
+        return {}
+    yql_url = baseurl + urlencode({'q': yql_query}) + "&format=json"
+    result = urlopen(yql_url).read()
+    data = json.loads(result)
+    res = makeWebhookResult(data)
+    return res
+
+
+def makeYqlQuery(req):
     result = req.get("result")
     parameters = result.get("parameters")
-    ctry = parameters.get("geo-country")
-    
-    url="https://raw.githubusercontent.com/cs109/2014_data/master/countries.csv"
-    s=requests.get(url).content
-    c=pd.read_csv(io.StringIO(s.decode('utf-8')))
-    df=pd.DataFrame(c)
+    city = parameters.get("geo-city")
+    if city is None:
+        return None
 
-   # cost = {'Europe':100, 'North America':200, 'South America':300, 'Asia':400, 'Africa':500}
+    return "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='" + city + "')"
 
-    speech = ctry + "is in " +  str(df.Region[df.Country==ctry]) + " continent"
+
+def makeWebhookResult(data):
+    query = data.get('query')
+    if query is None:
+        return {}
+
+    result = query.get('results')
+    if result is None:
+        return {}
+
+    channel = result.get('channel')
+    if channel is None:
+        return {}
+
+    item = channel.get('item')
+    location = channel.get('location')
+    units = channel.get('units')
+    if (location is None) or (item is None) or (units is None):
+        return {}
+
+    condition = item.get('condition')
+    if condition is None:
+        return {}
+
+    # print(json.dumps(item, indent=4))
+
+    speech = "Today in " + location.get('city') + ": " + condition.get('text') + \
+             ", the temperature is " + condition.get('temp') + " " + units.get('temperature')
 
     print("Response:")
     print(speech)
@@ -73,12 +107,10 @@ def makeWebhookResult(req):
     return {
         "speech": speech,
         "displayText": speech,
-        #"data": {},
+        # "data": data,
         # "contextOut": [],
-        "source": "apiai-continent-find"
+        "source": "apiai-weather-webhook-sample"
     }
-
-
 
 
 if __name__ == '__main__':
