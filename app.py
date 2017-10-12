427908f74a5c70a525be57c66da505e275de93a4
shahil1993 <shahil1993@gmail.com>
2017-10-12 15:33:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -58,7 +58,7 @@ def makeWebhookResult(req):
     parameters = result.get("parameters")
     ctry = parameters.get("geo-country")
     
-   url="https://raw.githubusercontent.com/cs109/2014_data/master/countries.csv"
+    url="https://raw.githubusercontent.com/cs109/2014_data/master/countries.csv"
     s=requests.get(url).content
     c=pd.read_csv(io.StringIO(s.decode('utf-8')))
     df=pd.DataFrame(c)
