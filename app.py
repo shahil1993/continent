4e38ae8abef1dda1ceaa6082bd2f849f62560621
shahil1993 <shahil1993@gmail.com>
2017-10-12 15:12:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,8 +15,8 @@
 # limitations under the License..
 
 from __future__ import print_function
-#from future.standard_library import install_aliases
-#install_aliases()
+from future.standard_library import install_aliases
+install_aliases()
 
 from urllib.parse import urlparse, urlencode
 from urllib.request import urlopen, Request
@@ -86,4 +86,4 @@ if __name__ == '__main__':
 
     print("Starting app on port %d" % port)
 
-    app.run(debug=False, port=port, host='01hw183219')
+    app.run(debug=False, port=port, host='0.0.0.0')
