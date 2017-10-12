e4d389bb1ff27eda5fa806b45d1138f5d4fe1260
shahil1993 <shahil1993@gmail.com>
2017-10-12 12:04:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -40,66 +40,29 @@ def webhook():
     print("Request:")
     print(json.dumps(req, indent=4))
 
-    res = processRequest(req)
+    res = makeWebhookResult(req)
 
     res = json.dumps(res, indent=4)
-    # print(res)
+    print(res)
     r = make_response(res)
     r.headers['Content-Type'] = 'application/json'
     return r
 
-
-def processRequest(req):
-    if req.get("result").get("action") != "yahooWeatherForecast":
+def makeWebhookResult(req):
+    if req.get("result").get("action") != "cont":
         return {}
-    baseurl = "https://query.yahooapis.com/v1/public/yql?"
-    yql_query = makeYqlQuery(req)
-    if yql_query is None:
-        return {}
-    yql_url = baseurl + urlencode({'q': yql_query}) + "&format=json"
-    result = urlopen(yql_url).read()
-    data = json.loads(result)
-    res = makeWebhookResult(data)
-    return res
-
-
-def makeYqlQuery(req):
     result = req.get("result")
     parameters = result.get("parameters")
-    city = parameters.get("geo-city")
-    if city is None:
-        return None
+    ctry = parameters.get("Country")
+    
+    url="https://raw.githubusercontent.com/cs109/2014_data/master/countries.csv"
+    s=requests.get(url).content
+    c=pd.read_csv(io.StringIO(s.decode('utf-8')))
+    df=pd.DataFrame(c)
 
-    return "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='" + city + "')"
+   # cost = {'Europe':100, 'North America':200, 'South America':300, 'Asia':400, 'Africa':500}
 
-
-def makeWebhookResult(data):
-    query = data.get('query')
-    if query is None:
-        return {}
-
-    result = query.get('results')
-    if result is None:
-        return {}
-
-    channel = result.get('channel')
-    if channel is None:
-        return {}
-
-    item = channel.get('item')
-    location = channel.get('location')
-    units = channel.get('units')
-    if (location is None) or (item is None) or (units is None):
-        return {}
-
-    condition = item.get('condition')
-    if condition is None:
-        return {}
-
-    # print(json.dumps(item, indent=4))
-
-    speech = "Today in " + location.get('city') + ": " + condition.get('text') + \
-             ", the temperature is " + condition.get('temp') + " " + units.get('temperature')
+    speech = ctry + "is in " +  str(df.Region[df.Country==ctry]) + " continent"
 
     print("Response:")
     print(speech)
@@ -107,10 +70,12 @@ def makeWebhookResult(data):
     return {
         "speech": speech,
         "displayText": speech,
-        # "data": data,
+        #"data": {},
         # "contextOut": [],
-        "source": "apiai-weather-webhook-sample"
+        "source": "apiai-continent-find"
     }
+
+
 
 
 if __name__ == '__main__':
